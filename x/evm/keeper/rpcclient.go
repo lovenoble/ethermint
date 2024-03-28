a80c27ee390458b388815eb8f55346d28fb279cb
Amaury <1293565+amaury1093@users.noreply.github.com>
2024-03-28 12:10:18 +0100

    wip

--- x/evm/keeper/rpcclient.go
+++ x/evm/keeper/rpcclient.go
@@ -7,6 +7,32 @@ import (
 	"github.com/ethereum/go-ethereum/core/vm"
 )
 
+type CallArgs struct {
+	caller vm.ContractRef
+	addr   common.Address
+	input  []byte
+	gas    uint64
+	value  *big.Int
+}
+
+type CallReply struct {
+	ret         []byte
+	leftOverGas uint64
+}
+
+type CreateArgs struct {
+	caller vm.ContractRef
+	code   []byte
+	gas    uint64
+	value  *big.Int
+}
+
+type CreateReply struct {
+	ret          []byte
+	contractAddr common.Address
+	leftOverGas  uint64
+}
+
 // EvmRpcClient is an interface for calling methods on the vm.EVM struct, which
 // is used to execute EVM transactions and lives in the SGX binary.
 type EvmRpcClient interface {
